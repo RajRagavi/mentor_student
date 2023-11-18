88353ee7b721f4ee474d54b161d778342d13c8c4
RagaviRajasekaran <143269843+RajRagavi@users.noreply.github.com>
2023-11-18 23:40:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+// this is for your reference
+//I used below links in postman all are worked
+//http://localhost:3000/mentors
+//http://localhost:3000/students/
+//http://localhost:3000/assign-student --Select one mentor and Add multiple Student
+//http://localhost:3000/students/:mentorId -- A student who has a mentor should not be shown in List 
+//http://localhost:3000/students/6558c672a6287f84f67f6149 -- Write API to show all students for a particular mentor
+//http://localhost:3000/change-mentor/ -- API to Assign or Change Mentor for particular Student
+//http://localhost:3000/previous-mentor/6558cba780895ea9399ee6d4 -- Write an API to show the previously assigned mentor for a particular student 
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
