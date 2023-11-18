f2bf04ad9798755e1abbf0a81b75c5117b20f2b1
RajRagavi <ragavirajasekaran2590@gmail.com>
2023-11-18 23:23:55 +0530

    task

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const PORT =  3000;
+
+app.use(bodyParser.json());
+app.use('/', routes);
+
+
+
+mongoose.connect('mongodb://localhost:27017/mentorStudentDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
